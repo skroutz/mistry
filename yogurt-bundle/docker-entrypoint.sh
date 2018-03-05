32cbd2fe25bbd2c670e5539d382de910f6cf22a3
Agis Anastasopoulos <agis.anast@gmail.com>
2018-03-05 17:22:48 +0200

    wip

--- yogurt-bundle/docker-entrypoint.sh
+++ yogurt-bundle/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "$GEMFILE_CONTENTS" > cache/Gemfile
-echo "$LOCKFILE_CONTENTS" > cache/Gemfile.lock
+echo "$GEMFILE" > cache/Gemfile
+echo "$LOCKFILE" > cache/Gemfile.lock
 
 bundle install --gemfile=tmp/Gemfile --deployment --path /data/artifacts
 bundle clean
