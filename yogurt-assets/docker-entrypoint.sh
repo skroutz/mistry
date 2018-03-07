2f96b9e2faf74d4fcaa288ffcc2c6d28e7fd579c
Agis Anastasopoulos <agis.anast@gmail.com>
2018-03-07 16:24:03 +0200

    Working yogurt-assets container + build-simulation script

--- yogurt-assets/docker-entrypoint.sh
+++ yogurt-assets/docker-entrypoint.sh
@@ -3,20 +3,18 @@ set -e
 
 ## these 2 will go away once we're on master
 git fetch
-git checkout builder-assets
-
-git pull
+git checkout origin/builder-assets
 
 bundle install # only run if needed
 yarn install
 script/lnconfs.rb
 script/mock_ymls.rb
-/bin/bash
 
 if [ -d /data/cache/sprockets_cache ]; then
 	mv /data/cache/sprockets_cache tmp
 fi
 
-#RAILS_ENV=production bundle exec rake assets:precompile
-rm -rf /data/cache/sprockets_cache
+RAILS_ENV=production bundle exec rake assets:precompile
+
 mv tmp /data/cache/sprockets_cache
+cp -r public/assets/* builds/artifacts/
