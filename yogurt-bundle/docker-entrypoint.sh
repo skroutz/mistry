05165a39af941fcfe29293f5ecd70df726427fa0
Agis Anastasopoulos <agis.anast@gmail.com>
2018-03-07 18:01:02 +0200

    Remove project files

--- yogurt-bundle/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#set -e
-#
-#echo "$GEMFILE" > cache/Gemfile
-#echo "$LOCKFILE" > cache/Gemfile.lock
-#
-#cd cache
-#bundle install --deployment --path /data/artifacts
-#bundle clean --force
