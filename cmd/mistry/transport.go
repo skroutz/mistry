a81938692376fcbd3016c91f20834e76f881f535
Dimitris Theodorou <dimitris.theodorou@gmail.com>
2018-04-30 12:45:30 +0300

    Add --clear-target option
    Closes #60

--- cmd/mistry/transport.go
+++ cmd/mistry/transport.go
@@ -2,23 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 type Transport interface {
-	Copy(user, host, project, src, dst string) []string
+	Copy(user, host, project, src, dst string, clearDst bool) []string
 }
 
 type Scp struct{}
 
-func (ts Scp) Copy(user, host, project, src, dst string) []string {
+func (ts Scp) Copy(user, host, project, src, dst string, clearDst bool) []string {
+	if clearDst {
+		removeDirContents(dst)
+	}
 	return []string{"scp", "-r", fmt.Sprintf("%s@%s:%s", user, host, src), dst}
+}
+
+func removeDirContents(dir string) error {
+	items, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range items {
+		err = os.RemoveAll(filepath.Join(dir, item.Name()))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 type Rsync struct{}
 
-func (ts Rsync) Copy(user, host, project, src, dst string) []string {
+func (ts Rsync) Copy(user, host, project, src, dst string, clearDst bool) []string {
 	module := "mistry"
 
 	idx := strings.Index(src, project)
@@ -26,6 +47,11 @@ func (ts Rsync) Copy(user, host, project, src, dst string) []string {
 		log.Fatalf("Expected '%s' to contain '%s'", src, project)
 	}
 	src = src[idx:]
+	cmd := []string{"rsync", "-rtlp"}
+	if clearDst {
+		cmd = append(cmd, "--delete")
+	}
+	cmd = append(cmd, fmt.Sprintf("%s@%s::%s/%s", user, host, module, src), dst)
 
-	return []string{"rsync", "-rtlp", fmt.Sprintf("%s@%s::%s/%s", user, host, module, src), dst}
+	return cmd
 }
